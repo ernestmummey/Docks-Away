066c4461d2a018c12378f27c300a0d5074a8c0aa
ernest_mummey <69600478+ernestmummey@users.noreply.github.com>
2021-12-06 10:11:36 -0500

    Initial install of react

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hopefully this start up</h1>
+    </div>
+  )
+};
+
+export default App;
