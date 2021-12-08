b5a00d35d6cc71f9a4c171eadca9111a91157ca6
ernest_mummey <69600478+ernestmummey@users.noreply.github.com>
2021-12-08 14:40:12 -0500

    Moved flask_sqlalchemy and config to config folder

--- flask-backend/backend/__init__.py
+++ flask-backend/backend/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import Flask, SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
+# import os 
 
 app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+# # disable signals as well as gaining extra memory
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
