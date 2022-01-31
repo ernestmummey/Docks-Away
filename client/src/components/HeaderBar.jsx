f47ff5271de0ba14e87bfbe51e7481f76c9e6e9a
ernest_mummey <69600478+ernestmummey@users.noreply.github.com>
2022-01-31 12:53:29 -0500

    Created HeaderBar component

---
+++ client/src/components/HeaderBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+
+
+const HeaderBar = () => {
+    return (
+        <Navbar className="py-3" bg="light">
+            <Container className="mx-4 mw-100">
+                <Navbar.Brand className="fs-2 fw-bold" href="#home">DocksAway</Navbar.Brand>
+                <Navbar.Collapse className="justify-content-end">
+                <Navbar.Text className="fs-5">
+                    Signed in as: <a href="#login">Mark Otto</a>
+                </Navbar.Text>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default HeaderBar;
