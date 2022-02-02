793067350a994efb4a5264cca3cb3307ad746da8
ernest_mummey <69600478+ernestmummey@users.noreply.github.com>
2022-02-02 12:00:06 -0500

    Imported LaunchTable and BoatsOutTable

--- client/src/views/Landing.jsx
+++ client/src/views/Landing.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import HeaderBar from '../components/HeaderBar'
+import LaunchTable from '../components/LaunchTable'
+import BoatsOutTable from '../components/BoatsOutTable'
+
 
 const Landing = () => {
     return (
         <div>
             <HeaderBar/>
+            <LaunchTable/>
+            <BoatsOutTable/>
         </div>
     )
 };
