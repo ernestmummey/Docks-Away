5adfcec2be1051c7c464e2c22d7ceb4900c3d524
ernest_mummey <69600478+ernestmummey@users.noreply.github.com>
2022-03-27 20:16:58 -0400

    Created a baots out view to add more components

---
+++ client/src/views/BoatsOutView.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BoatsOutTable from '../components/BoatsOutTable';
+
+
+
+const BoatsOut = () => {
+    return (
+        <div>
+            <BoatsOutTable/>
+        </div>
+    )
+}
+
+export default BoatsOut;
