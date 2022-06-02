fc89566a8741e4d590661e66dbbe714f325156e7
ernest_mummey <69600478+ernestmummey@users.noreply.github.com>
2022-06-01 21:40:16 -0400

    Created nav bar

---
+++ client/src/components/BoatLaunchNavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+
+const BoatLaunchNavBar = () => {
+    return (
+        <div>
+            <Navbar className="py-3" bg="light">
+            <Container className="mx-5 mw-100">
+                <Navbar.Brand className="fs-2 fw-bold" href="#home">DocksAway</Navbar.Brand>
+                <Navbar.Collapse className="justify-content-end">
+                <Navbar.Text className="fs-5">
+                    Signed in as: <a href="#login">Mark Otto</a>
+                </Navbar.Text>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        </div>
+    )
+}
+
+
+export default BoatLaunchNavBar;
