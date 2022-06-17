3d0ef6ce25ad0c5996701c9700ba58e579c8091b
ernest_mummey <69600478+ernestmummey@users.noreply.github.com>
2022-06-16 21:10:38 -0400

    Created simple header

--- client/src/components/DeskTopHeader/index.jsx
+++ client/src/components/DeskTopHeader/index.jsx
@@ -3,8 +3,11 @@ import './index.scss'
 
 const DeskTopHeader = () => {
     return (
-        <>
-        </>
+        <div className='nav-bar'>
+            <div className='logo'>
+                <h1>DocksAway</h1>
+            </div>
+        </div>
     )
 };
 
