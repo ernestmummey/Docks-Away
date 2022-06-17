0f7bde0f94e2ae751e60a4c214b37c394e2d08b6
ernest_mummey <69600478+ernestmummey@users.noreply.github.com>
2022-06-17 12:27:20 -0400

    Created a dumie data table for refrance

---
+++ client/src/components/ListTable/index.jsx
@@ -0,0 +1,32 @@
+import './index.scss'
+
+
+
+const ListTable = () => {
+    return (
+        <div className='table'>
+            <thead>
+                <tr>
+                    {/* map */}
+                    <th>fill data</th>
+                    <th>fill data</th>
+                    <th>fill data</th>
+                    <th>fill data</th>
+                    <th>fill data</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    {/* map */}
+                    <th>data</th>
+                    <th>data</th>
+                    <th>data</th>
+                    <th>data</th>
+                    <th>data</th>
+                </tr>
+            </tbody>
+        </div>
+    )
+};
+
+export default ListTable;
