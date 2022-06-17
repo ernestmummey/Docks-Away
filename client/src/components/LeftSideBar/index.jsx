7586c1cd8f9e120b876b63872a9ac4f218d1c7e6
ernest_mummey <69600478+ernestmummey@users.noreply.github.com>
2022-06-16 21:10:59 -0400

    Started to work on the side bar

--- client/src/components/LeftSideBar/index.jsx
+++ client/src/components/LeftSideBar/index.jsx
@@ -2,8 +2,13 @@ import './index.scss'
 
 const LeftSideBar = () => {
     return (
-        <>
-        </>
+        <div className='side-bar'>
+            <ul className='side-bar-list'>
+                <li>Launch Boat</li>
+                <li>Stow Boat</li>
+                <li>Marina View</li>
+            </ul>
+        </div>
     )
 };
 
