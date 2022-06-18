a2d0a6332381afbb5e7e9562cb3371ae06038ada
ernest_mummey <69600478+ernestmummey@users.noreply.github.com>
2022-06-18 18:31:12 -0400

    added two divs to allow the set up f the page

--- client/src/components/Layout/index.jsx
+++ client/src/components/Layout/index.jsx
@@ -1,14 +1,24 @@
-
 import DeskTopHeader from '../DeskTopHeader';
 import LeftSideBar from '../LeftSideBar'
+import ListTable from '../ListTable';
 import './index.scss'
 
 
 const Layout = () => {
     return (
         <div className='App'>
-            <DeskTopHeader />
-            <LeftSideBar />
+            <div className='header-container'>
+                <DeskTopHeader />
+            </div>
+            <div className='landing'>
+                <div className='sidebar-container'>
+                    <LeftSideBar />
+                </div>
+                <div className='page'>
+                        <ListTable />
+                        <ListTable />
+                </div>
+            </div>
         </div>
     )
 };
